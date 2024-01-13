9df3fcffea0701b8f1e372abd73c113b9b4bf913
Suresh Chand <sureshchand12a@gmail.com>
2024-01-13 20:05:30 +0545

    Fix Domain

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
-
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
+    output: 'export'
 }
 
 module.exports = nextConfig
