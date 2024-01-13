4b4f63d187679a78049a05b972741a59975b8b87
Suresh Chand <sureshchand12a@gmail.com>
2024-01-13 21:06:59 +0545

    Fixed Color Constrait

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -16,12 +16,12 @@ export default function Education() {
                             {
                                 education.map((edu) => (
                                     <li className="mb-10 ms-6" key={edu.id}>
-                                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white ring-gray-900 bg-blue-900">
+                                        <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-gray-900 bg-blue-900">
                                             <svg className="w-2.5 h-2.5 text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                                             </svg>
                                         </span>
-                                        <h3 className="flex items-center mb-1 text-lg font-semibold text-white">{edu.school} {edu.running && <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded bg-blue-900 text-blue-300 ms-3">Running</span>}</h3>
+                                        <h3 className="flex items-center mb-1 text-lg font-semibold text-white">{edu.school} {edu.running && <span className="bg-blue-300 text-sm font-medium me-2 px-2.5 py-0.5 rounded text-blue-900 ms-3">Running</span>}</h3>
                                         <time className="block mb-2 text-sm font-normal leading-none text-gray-500">{edu.degree}</time>
                                         <p className="mb-4 text-base font-normal text-gray-400">{edu.desc}</p>
                                     </li>
